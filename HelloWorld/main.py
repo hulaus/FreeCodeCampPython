92f0369658e42256498ad4fba613e30e5085ef9e
Jo Carr <hulaus21@gmail.com>
2023-08-29 18:54:47 -0700

    added new functions to main.py and removed original code

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -1,9 +1,19 @@
-from library import greet
-
 def main():
-    greet()
+    book = 'Dracula'
+    author = 'Bram Stoker'
+    release_year = 1897
+    goodreads_rating = 4.01
+
+    print(book)
+    print(author)
+    print(release_year)
+    print(goodreads_rating)
 
 if __name__ == '__main__':
     main()
 
-# Hello, World !
+
+# Dracula
+# Bram Stoker
+# 1897
+# 4.01
