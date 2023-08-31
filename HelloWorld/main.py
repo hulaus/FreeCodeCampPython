c4ca8296299070e6d4b8be01f0fe98fcc855fa4a
Jo Carr <hulaus21@gmail.com>
2023-08-30 21:59:55 -0700

    added + in between each variable to make strings in between the variables to form the sentence shown when it is run

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -2,9 +2,9 @@
 ## Variable names follow the same convention as fucntion names
 
 def main():
-    book, author, release_year, goodreads_rating = 'Dracula', 'BramvStoker', '1897', '4.01'
+    book, author, release_year, goodreads_rating = 'Dracula', 'Bram Stoker', '1897', '4.01'
 
-    print(book, author, release_year, goodreads_rating)
+    print(book + ' is a novel by ' + author  + ', published in ' + release_year +'. It has a rating of ' + goodreads_rating + ' on goodreads.')
 
 if __name__ == '__main__':
     main()
