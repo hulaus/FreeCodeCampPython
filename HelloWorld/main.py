8fbe36192f6dc4db137650910617d79048039251
Jo Carr <hulaus21@gmail.com>
2023-08-30 22:09:38 -0700

    changed line of code using an f string putting variables into {} so the line of code is more readable

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -4,7 +4,7 @@
 def main():
     book, author, release_year, goodreads_rating = 'Dracula', 'Bram Stoker', '1897', '4.01'
 
-    print(book + ' is a novel by ' + author  + ', published in ' + release_year +'. It has a rating of ' + goodreads_rating + ' on goodreads.')
+    print(f' {book} is a novel by  {author}, published in {release_year}. It has a rating of {goodreads_rating} on goodreads.')
 
 if __name__ == '__main__':
     main()
