24eed7473b1ac9c8dc053fe3b25a359a841c87cd
Jo Carr <hulaus21@gmail.com>
2023-08-30 21:51:50 -0700

    changed values from separate prints to one print with multiple variable in a line separated by commas

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -2,20 +2,12 @@
 ## Variable names follow the same convention as fucntion names
 
 def main():
-    book = 'Dracula'
-    author = 'Bram Stoker'
-    release_year = 1897
-    goodreads_rating = 4.01
+    book, author, release_year, goodreads_rating = 'Dracula', 'BramvStoker', '1897', '4.01'
 
-    print(book)
-    print(author)
-    print(release_year)
-    print(goodreads_rating)
+    print(book, author, release_year, goodreads_rating)
 
 if __name__ == '__main__':
     main()
-
-
 # Dracula
 # Bram Stoker
 # 1897
