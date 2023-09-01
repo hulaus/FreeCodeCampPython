7103fa289d1ee8f76960d35a9691a694b88a9955
Jo Carr <hulaus21@gmail.com>
2023-08-31 20:59:49 -0700

    split the line of code into 2 f strings

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -4,7 +4,8 @@
 def main():
     book, author, release_year, goodreads_rating = 'Dracula', 'Bram Stoker', '1897', '4.01'
 
-    print(f' {book} is a novel by  {author}, published in {release_year}. It has a rating of {goodreads_rating} on goodreads.')
+    print(f' {book} is a novel by  {author}, published in {release_year}.'
+          f' It has a rating of {goodreads_rating} on goodreads.')
 
 if __name__ == '__main__':
     main()
