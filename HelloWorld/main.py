93ebd89b43e8134768b0515e9647625d218b9877
Jo Carr <hulaus21@gmail.com>
2023-09-03 22:34:46 -0700

    used the pow(x, y) method to find the square of 2

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -2,10 +2,15 @@
 #of the simple numeric types. The simplest of the bunch are addition and subtraction
 
 def main():
-    num_1 = -5.8
+   x = 2
+   y = 3
 
-    print(f'the absolute value of {num_1} is: {abs(num_1)}')
+   print(f'{2} to the power of {3} is: {pow(2,3)}')
+   print(f'{2} to the power of {3} is: {2 ** 3}')
+
 
 if __name__ == '__main__':
     main()
 
+# 2 to the power of 3 is: 8 
+# 2 to the power of 3 is :8
